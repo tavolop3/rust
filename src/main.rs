48b2c4bfb400c224c6775b00483acf6201337546
tavo <tavo.lop33@gmail.com>
2025-04-22 13:59:14 -0300

    hasta el 5 inclusive

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 mod tp02;
-use tp02::ej1::resuelve;
 
 fn main() {
-    println!("Hello, world!");
-    tp02::ej1::resuelve();
-    resuelve();
+    let num1 = 2;
+    let is_even = tp02::ej01::is_even(num1);
+    println!("ej1 -> num:{num1}, is even? {is_even}");
+
+    let num2 = 4;
+    let is_prime = tp02::ej02::is_prime(num2);
+    println!("ej2 -> num:{num2}, is prime? {is_prime}");
+
+    let arr: [u64; 5] = [1, 2, 3, 4, 5];
+    let sum = tp02::ej03::sum_arr(arr);
+    println!("ej3 -> arr:{arr:?}, sum:{sum}");
+
+    let n_odds = tp02::ej04::n_odds(arr);
+    println!("ej4 -> arr:{arr:?}, num odds:{n_odds}");
+
+    let arrf: [f64; 5] = [1.1, 5.5, 3.3, 4.4, 5.5];
+    let dup_arr = tp02::ej05::dup_vals(arrf);
+    println!("ej5 -> arr:{arrf:?}, new arr:{dup_arr:?}");
+
+    
 }
--- src/tp02/ej05.rs
+++ src/tp02/ej05.rs
@@ -1,7 +1,14 @@
-pub fn dup_arr(arr: [f64; 5]) -> Vec<f64> {
-    for i in arr.len() {
-        for j in arr - i {}
+pub fn dup_vals(arr: [f64; 5]) -> [f64;5] {
+    let mut dups:[f64; 5] = [0.0;5];
+    let mut dup_i = 0;
+    for i in 0..arr.len() {
+        for j in i+1..arr.len() {
+            if arr[i] == arr[j] {
+                dups[dup_i] = arr[i];
+                dup_i += 1;
+            }
+        }
     }
 
-    return res;
+    return dups;
 }
